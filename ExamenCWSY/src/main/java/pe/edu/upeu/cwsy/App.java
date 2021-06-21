b9960abd7e6bc572f7eaa3d197b21cddd3d63cdb
ChristianSalas1 <>
2021-06-21 17:57:10 +0000

    examenprogramacion

---
+++ ExamenCWSY/src/main/java/pe/edu/upeu/cwsy/App.java
@@ -0,0 +1,18 @@
+package pe.edu.upeu.cwsy;
+
+import pe.edu.upeu.cwsy.examen.ResolucionExamen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+       ResolucionExamen obj=new ResolucionExamen();
+      //obj.impuestoVehiculos();
+      //obj.MultiplicaciondeunNumeroENtero();
+      //obj.DeterminaciondenumerosPerfectos();
+      //System.out.println(obj.potencia(4,10));
+       
+    }
+}
